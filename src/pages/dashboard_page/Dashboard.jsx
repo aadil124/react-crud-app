9e50a85f89b33324a588badce7dfb156e19212fe
aadil124 <ansariaadil124@gmail.com>
2023-07-28 00:00:32 +0530

    updated functionality

--- src/pages/dashboard_page/Dashboard.jsx
+++ src/pages/dashboard_page/Dashboard.jsx
@@ -1,11 +1,13 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { RxAvatar } from "react-icons/rx";
+import { HiEye, HiPencil, HiTrash, HiUserCircle } from "react-icons/hi2";
 import { BsEye } from "react-icons/bs";
 import { GrEdit } from "react-icons/gr";
 import { RiDeleteBin6Line } from "react-icons/ri";
 import {
   Button,
+  ButtonGroup,
+  ButtonToolbar,
   ListGroup,
   ListGroupItem,
   ListGroupItemHeading,
@@ -15,67 +17,104 @@ import {
 import { Link } from "react-router-dom";
 
 const Dashboard = () => {
-  const url = "https://dev-73841661c741363.api.raw-labs.com/your/endpoint/path";
-
+  const [id, setId] = useState();
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
-  const getDataFromURL = async () => {
+  const [deleteLoading, setDeleteLoading] = useState(false);
+  const getUserData = async () => {
+    const url =
+      "https://dev-73841661c741363.api.raw-labs.com/your/endpoint/path";
     try {
       await axios.get(url).then(({ data }) => {
         console.log(data);
         setUsers(data);
       });
+      setLoading(false);
     } catch (error) {
       console.log(error);
     }
-    setLoading(false);
   };
 
   useEffect(() => {
-    getDataFromURL();
-  }, [url]);
+    getUserData();
+  }, []);
 
   //functionality for delete user
 
-  const deleteUser = (index) => {
-    const filteredUser = users.filter(({ id }) => id !== index);
-    console.log("filteredUser", filteredUser);
-    setUsers(filteredUser);
+  const deleteUser = async (userId) => {
+    const url = `https://dev-73841661c741363.api.raw-labs.com/your/endpoint/path/${userId}`;
+    setDeleteLoading(true);
+    setId(userId);
+    try {
+      await axios.delete(url);
+      getUserData();
+      setDeleteLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
-    <ListGroup className="m-5">
-      {loading ? (
-        <Spinner>Loading...</Spinner>
-      ) : (
-        <>
-          <ListGroupItem className="d-flex justify-content-between p-3">
-            <span className="fs-4 fw-bold">User List</span>
-            <Button color="primary">Add User +</Button>
-          </ListGroupItem>
-          {users.map((item, index) => (
-            <ListGroupItem key={index} className="d-flex align-items-center">
-              <RxAvatar className="fs-1 mx-3" />
-              <div className="p-2">
-                <ListGroupItemHeading>{item.name}</ListGroupItemHeading>
-                <ListGroupItemText>{item.email}</ListGroupItemText>
-              </div>
-              <div className="mx-auto">
-                <Link to="/profile">
-                  <BsEye color="green" className="mx-3" />
-                </Link>
-                <Link to="/edit">
-                  <GrEdit color="blue" className="mx-3" />
-                </Link>
-                <Button color="transparent" onClick={() => deleteUser(item.id)}>
-                  <RiDeleteBin6Line color="red" className="mx-3" />
-                </Button>
-              </div>
+    <div className="d-flex my-4 container-fluid col-sm-6 offset-sm-2 offset-lg-4">
+      <ListGroup>
+        {loading ? (
+          <Spinner className="m-5" color="dark">
+            Loading...
+          </Spinner>
+        ) : (
+          <>
+            <ListGroupItem className="d-flex justify-content-between w-100 m-3">
+              <span className="fs-4 fw-bold my-auto">User List</span>
+              <Link to="/create">
+                <Button color="primary">Add User +</Button>
+              </Link>
             </ListGroupItem>
-          ))}
-        </>
-      )}
-    </ListGroup>
+            {users.map((user) => (
+              <ListGroupItem
+                key={user.id}
+                className="d-flex justify-content-between"
+              >
+                <div className="d-flex me-5">
+                  <Button color="none" className="outline-none">
+                    <Link to={"/profile" + user.id}>
+                      <HiUserCircle className="fs-3 mx-3 my-auto text-dark" />
+                    </Link>
+                  </Button>
+                  <div className="p-2">
+                    <div>{user.name}</div>
+                    <span className="text-primary">{user.email}</span>
+                  </div>
+                </div>
+                <ButtonToolbar>
+                  <ButtonGroup className="me-2">
+                    <Button color="link">
+                      <Link to={"/profile/" + user.id}>
+                        <HiEye color="green" />
+                      </Link>
+                    </Button>
+                    <Button color="link">
+                      <Link to={"/edit/" + user.id}>
+                        <HiPencil color="blue" />
+                      </Link>
+                    </Button>
+                    <Button color="link">
+                      {id === user.id && deleteLoading ? (
+                        <Spinner color="danger">Loading...</Spinner>
+                      ) : (
+                        <HiTrash
+                          color="red"
+                          onClick={() => deleteUser(user.id)}
+                        />
+                      )}
+                    </Button>
+                  </ButtonGroup>
+                </ButtonToolbar>
+              </ListGroupItem>
+            ))}
+          </>
+        )}
+      </ListGroup>
+    </div>
   );
 };
 

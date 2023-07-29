fb6b6599fefccdca3ffc8ede915a903edbaa979f
aadil124 <ansariaadil124@gmail.com>
2023-07-29 20:17:14 +0530

    updated create user

--- src/pages/create_page/CreateUser.jsx
+++ src/pages/create_page/CreateUser.jsx
@@ -1,7 +1,92 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Form, FormGroup, Input, Label, Spinner } from "reactstrap";
 
 const CreateUser = () => {
-  return <div>CreateUser</div>;
+  const [createUser, setCreateUser] = useState({
+    name: "",
+    email: "",
+    department: "",
+    country: "",
+  });
+  const [createLoading, setCreateLoading] = useState(false);
+  const navigate = useNavigate();
+  // console.log(navigate);
+  const handleChange = (e) => {
+    setCreateUser({ ...createUser, [e.target.name]: e.target.value });
+  };
+
+  const createHandle = async () => {
+    const url = `https://dev-73841661c741363.api.raw-labs.com/your/endpoint/path`;
+    setCreateLoading(true);
+    try {
+      await axios.post(url, createUser);
+      navigate("/");
+      setCreateLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Form className="mt-5 w-50 m-auto">
+      <FormGroup>
+        <Label for="name">User Name</Label>
+        <Input
+          id="name"
+          name="name"
+          placeholder="Enter Name"
+          type="text"
+          value={createUser.name}
+          onChange={handleChange}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="email">Email</Label>
+        <Input
+          id="email"
+          name="email"
+          placeholder="Enter Email"
+          type="email"
+          value={createUser.email}
+          onChange={handleChange}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="department">Department</Label>
+        <Input
+          id="department"
+          name="department"
+          placeholder="Enter department"
+          type="text"
+          value={createUser.department}
+          onChange={handleChange}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Label for="country">Country</Label>
+        <Input
+          id="country"
+          name="country"
+          placeholder="Enter Country Name"
+          type="text"
+          value={createUser.country}
+          onChange={handleChange}
+        />
+      </FormGroup>
+      {createLoading ? (
+        <Button color="primary" disabled>
+          <Spinner size="sm">Loading...</Spinner>
+          <span> Creating...</span>
+        </Button>
+      ) : (
+        <Button color="primary" onClick={createHandle}>
+          Create
+        </Button>
+      )}
+    </Form>
+  );
 };
 
 export default CreateUser;

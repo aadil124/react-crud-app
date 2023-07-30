0ad27dcec29acb13098d06a66f8da03945eb7f30
aadil124 <ansariaadil124@gmail.com>
2023-07-31 00:51:09 +0530

    user not found added

--- src/components/UserNotFound.jsx
+++ src/components/UserNotFound.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import PageNotFound from "../assets/404-page-not-found.png";
+import { Link } from "react-router-dom";
+import { Button } from "reactstrap";
 
 const UserNotFound = () => {
-  return <div>User Not Found</div>;
+  return (
+    <div style={{ textAlign: "center" }}>
+      <img src={PageNotFound} />
+      <div>
+        <Link to="/">
+          <Button color="primary mt-3">Go to Home</Button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default UserNotFound;
